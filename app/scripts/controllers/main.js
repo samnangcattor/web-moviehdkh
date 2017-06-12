32a99ffd1cac3bd871d6b6e1cea80c01d334795f
samnangcattor <kcattor@gmail.com>
2017-06-12 10:47:34 +0700

    first comment

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webMoviehdkhApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webMoviehdkhApp
+ */
+angular.module('webMoviehdkhApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
